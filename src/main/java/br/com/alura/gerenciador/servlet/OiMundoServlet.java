1f324f143ecf9825030d2c86d6eeefcaf7cc3201
unknown <gabriel_pinhatarubio@hotmail.com>
2023-02-21 21:46:42 -0300

    Conhecendo o webxml

--- src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-//oi
 @WebServlet(urlPatterns = "/oi")
 public class OiMundoServlet extends HttpServlet{
 	
